d1d4ff553c36bd3da38165623bc835c063451646
sarban <sarban7@mail.ru>
2025-09-04 22:44:27 +0300

    2 -Using Unity Behavior+

--- Assets/Scripts/Units/AbstractUnit.cs
+++ Assets/Scripts/Units/AbstractUnit.cs
@@ -1,15 +1,18 @@
+using Unity.Behavior;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Rendering.Universal;
 
-[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(NavMeshAgent), typeof(BehaviorGraphAgent))]
 public abstract class AbstractUnit : AbstractCommandable, IMoveable
 {
     public float AgentRadius => agent.radius;
     private NavMeshAgent agent;
+    private BehaviorGraphAgent agentGraph;
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        agentGraph = GetComponent<BehaviorGraphAgent>();
     }
 
     protected override void Start()
@@ -21,6 +24,6 @@ public abstract class AbstractUnit : AbstractCommandable, IMoveable
 
     public void MoveTo(Vector3 position)
     {
-        agent.SetDestination(position);
+        agentGraph.SetVariableValue("TargetLocation", position);
     }
 }
